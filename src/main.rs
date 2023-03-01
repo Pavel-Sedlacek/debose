55b140c58f923159c5ac4d5505929f8cbc577d31
Pavel Sedláček <pavel.sedlacek2004@gmail.com>
2023-03-01 09:31:23 +0100

    hodne improve

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 #![feature(iter_array_chunks)]
 #![feature(slice_flatten)]
 
-use std::{fs::File, io::read_to_string, time::Instant};
+use std::{
+    fs::File,
+    io::read_to_string,
+    time::{Duration, Instant},
+};
 
 use regex::Regex;
-use rusqlite::{Connection, Map};
+use rusqlite::{Connection, Map, Statement};
+
+use rand::Rng;
 
 fn main() {
     let mut search = String::new();
@@ -30,12 +36,18 @@ fn main() {
 
     let mut queries = [
         connection
-            .prepare("SELECT id, ip_start, ip_end, country, stateprov, city FROM ips NOT INDEXED WHERE ip_start <= ?1 AND ip_end >= ?2")
+            .prepare("SELECT id, ip_start, ip_end, country, stateprov, city FROM ips NOT INDEXED WHERE ip_start <= ?1 AND ip_end >= ?2 LIMIT 1")
             .unwrap(),
         connection
-            .prepare("SELECT id, ip_start, ip_end, country, stateprov, city FROM ips WHERE ip_start <= ?1 AND ip_end >= ?2")
+            .prepare("SELECT id, ip_start, ip_end, country, stateprov, city FROM ips WHERE ip_start <= ?1 AND ip_end >= ?2 LIMIT 1")
             .unwrap()
     ];
+
+    println!("Run benchmarks?");
+    stdin.read_line(&mut search).expect("Something went wrong!");
+    if search.to_lowercase().contains("y") {
+        benchmark(&connection, &mut queries);
+    }
 
     loop {
         println!("enter ip between {} and {}", u32::MIN, u32::MAX);
@@ -48,13 +60,20 @@ fn main() {
 
         let ip = string_to_ip(search.trim().to_string());
 
-        println!("searching for {ip}");
+        println!(
+            "\n\n... searching for ip: {ip} (dec) = {} (bin) ...",
+            search.trim()
+        );
 
         let now = Instant::now();
 
         let fetch = queries[if ip < 1216605195 { 0 } else { 1 }].query([ip, ip]);
 
-        println!("Fetched in: {}ns", now.elapsed().as_nanos());
+        println!(
+            "Fetched in:              {: >12}ns ~ {: >12}ms",
+            now.elapsed().as_nanos(),
+            now.elapsed().as_nanos() as f64 / 1_000_000.0
+        );
 
         let data = fetch.unwrap().mapped(|row| {
             Ok(Record {
@@ -72,7 +91,55 @@ fn main() {
             data.map(|a| format!("{}", a.unwrap().country.clone()))
                 .collect::<Vec<String>>()
         );
+
+        println!(
+            "Fetched and parsed in:   {: >12}ns ~ {: >12}ms",
+            now.elapsed().as_nanos(),
+            now.elapsed().as_nanos() as f64 / 1_000_000.0
+        );
+        println!("");
     }
+}
+
+fn benchmark(connection: &Connection, queries: &mut [Statement; 2]) {
+    let mut fetch: Vec<Duration> = Vec::new();
+    let mut parse: Vec<Duration> = Vec::new();
+
+    (0..u32::MAX).step_by(32).for_each(|ip| {
+        // let ip = rand::thread_rng().gen_range(0..u32::MAX);
+
+        let start = Instant::now();
+
+        let f = queries[if ip < 1216605195 { 0 } else { 1 }].query([ip, ip]);
+
+        fetch.push(start.elapsed());
+
+        let data = f.unwrap().mapped(|row| {
+            Ok(Record {
+                id: row.get(0)?,
+                ip_start: row.get(1)?,
+                ip_end: row.get(2)?,
+                country: row.get(3)?,
+                stateprov: row.get(4)?,
+                city: row.get(5)?,
+            })
+        });
+
+        parse.push(start.elapsed());
+    });
+
+    println!(
+        "Average fetch: {}ns, worst fetch: {}ns, best fetch: {}ns",
+        fetch.iter().map(|i| i.as_nanos()).sum::<u128>() as f64 / fetch.len() as f64,
+        fetch.iter().max().unwrap().as_nanos(),
+        fetch.iter().min().unwrap().as_nanos()
+    );
+    println!(
+        "Average parse: {}ns, worst parse: {}ns, best parse: {}ns",
+        parse.iter().map(|i| i.as_nanos()).sum::<u128>() as f64 / parse.len() as f64,
+        parse.iter().max().unwrap().as_nanos(),
+        parse.iter().min().unwrap().as_nanos()
+    );
 }
 
 fn sql_shell(connection: &Connection) {
